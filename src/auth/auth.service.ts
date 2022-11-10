9a087b59669ff53aaf5c82609ff7063929f24a51
fepego-penag <cf.pena1138@uniandes.edu.co>
2022-11-09 21:52:39 -0500

    fix bugs from sonar (#26)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user: User = await this.usersService.findOne(username);
-    if (user & user.password === password) {
+    if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
